7a16221597bdb6d42f8ac2c79f19c9145fce6ebd
Bryan <Bryan_miranda@ricaldone.edu.sv>
2024-03-19 13:44:16 -0600

    app2

--- app/src/main/java/fernanda/hernandez/miappfer/MainActivity.kt
+++ app/src/main/java/fernanda/hernandez/miappfer/MainActivity.kt
@@ -3,6 +3,8 @@ package fernanda.hernandez.miappfer
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         val txtnum1 =  findViewById<EditText>(R.id.txtnum1)
         val txtnum2 =  findViewById<EditText>(R.id.txtnum2)
         val btnsum =  findViewById<Button>(R.id.btnsum)
+        val txtresultado =  findViewById<TextView>(R.id.txtresultados)
 
        //creo objeto para mandar a llamar el objeto de la calcu
 
@@ -31,7 +34,11 @@ class MainActivity : AppCompatActivity() {
 
         //programar
         btnsum.setOnClickListener {
-            objCalculadora.sumar(txtnum1.text.toString().toInt(), txtnum2.text.toString().toInt())
+           val result =  objCalculadora.sumar(txtnum1.text.toString().toInt(), txtnum2.text.toString().toInt())
+            Toast.makeText( this, "Este es el resultado $result", Toast.LENGTH_SHORT).show()
+
+            txtresultado.text = "El resultado es: $result"
+
 
 
         }
